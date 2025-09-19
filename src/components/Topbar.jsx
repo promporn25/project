3603a8a63306324f42d2d87194664031dbb3db1b
unknown <you@example.com>
2025-09-19 18:40:05 +0700

    อัปเดตโค้ดล่าสุด

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,16 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Topbar() {
+  const nav = useNavigate(); // ✅ เพิ่ม useNavigate
+
   return (
-    <div className="topbar">
-      {/* ปุ่มไปหน้าโปรไฟล์ */}
+    <header className="topbar">
+      <div className="logo" onClick={() => nav("/home")}>
+        GRADIE <span className="ku">KU</span>
+      </div>
+
       <Link to="/info" className="profile" title="โปรไฟล์">
-        <img 
-          src="/images/10.png"  // 🔹 path รูป
-          alt="โปรไฟล์" 
-          style={{ width: "40px", height: "40px", borderRadius: "50%" }} // 🔹 ตั้งขนาดและให้เป็นวงกลม
+        <img
+          src="/images/10.png"
+          alt="โปรไฟล์"
+          style={{ width: "40px", height: "40px", borderRadius: "50%" }}
         />
       </Link>
-    </div>
+    </header>
+    
   );
 }
--- src/pages/Frist.jsx
+++ src/pages/Frist.jsx
@@ -2,27 +2,31 @@ import Topbar from "../components/Topbar.jsx";
 import { useNavigate } from "react-router-dom";
 import '../styles.css';
 
-
-
 export default function Home() {
   const nav = useNavigate();
+  
   return (
     <>
       <Topbar />
       <div className="blue-box">
+        
+        {/* Card 1 */}
         <div className="card-container">
           <div className="card" style={{animationDelay:"0.2s"}}>
-            <img src="/image/1.png" alt="explore" style={{ width: "400px" }} />
+            <h2>Curriculum Courses</h2>
             <p>Take a moment to explore the courses and find the one that’s perfect for you.</p>
+            <img src="/image/1.png" alt="explore" />
             <button onClick={() => nav("/study-plans")}>EXPLORE!</button>
           </div>
           <div className="label">CURRICULUM OVERVIEW</div>
         </div>
 
+        {/* Card 2 */}
         <div className="card-container">
           <div className="card" style={{animationDelay:"0.4s"}}>
-            <img src="/image/2.png" alt="check" style={{ width: "400px" }} />
+            <h2>Check Curriculum Courses</h2>
             <p>Review the courses you have completed and easily check the remaining ones here</p>
+            <img src="/image/2.png" alt="check" />
             <button onClick={() => nav("/check-subject")}>CHECK IT!</button>
           </div>
           <div className="label">CHECK CURRICULUM COURSES</div>
